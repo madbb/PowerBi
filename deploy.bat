ae92c1149667f41fa05917df7a4b692b2d849031
Marcel Döbbel <Marcel.Doebbel@enders-training.de>
2026-05-29 12:11:28 +0200

    update 29.05.2026 12:11:28,16

--- deploy.bat
+++ deploy.bat
@@ -2,9 +2,10 @@
 cd /d "C:\Users\marce\Documents\Arbeit\PowerBi\PowerBI Projektordner\pbi-kurs"
 
 echo Baue Jupyter Book...
-jupyter-book build .
+jupyter book build --html
 
 echo Pushe zu GitHub...
+git pull
 git add .
 git commit -m "update %date% %time%"
 git push
